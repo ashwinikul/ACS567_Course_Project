d8704636d931eab0ce5c6adb867f3d5096713aa6
ashwinikul <kulka02@students.ipfw.edu>
2017-04-23 13:39:03 -0400

    Update index1.php

--- src/index1.php
+++ src/index1.php
@@ -12,10 +12,12 @@
         $connection = connect_to_db();
         
         // prepare SQL
+	     //updated <19 APR 2017 >    added condition DT.testname='MBTI'   
         $sql = sprintf("SELECT DQ.qusid, DT.Testid, DQ.qus_desc, OPT.op1, OPT.op2
                             From Dim_Question DQ
                             INNER JOIN  Dim_Test DT ON DQ.testid= DT.testid
                             INNER JOIN Dim_Qus_Option OPT ON (DQ.testid=OPT.testid and DQ.qusid=OPT.qusid)
+			    WHERE DT.testname='MBTI'
                             ORDER BY DQ.qusid;
                             ");
         
