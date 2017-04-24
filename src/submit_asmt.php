3f33a1acbb54fd22c6cc7b863edf8d1741068d5e
Sindhu Balakrishnan <balas01@students.ipfw.edu>
2017-04-24 23:11:30 +0000

    After integration

--- src/submit_asmt.php
+++ src/submit_asmt.php
@@ -1,39 +1,34 @@
-/* 
-	Developer: Sindhu Balakrishnan
-	Description: PHP page for submitting MBTI questionnaire. 
-
-*/
 <?php
+//	Developer: Sindhu Balakrishnan
+//	Description: PHP file to submit an assessment details.
+
     require_once('db_conn.php');
     
     function submit_asmt()
         {
             try {
-                    echo "U r here";
                     $connection = connect_to_db();
 
-                    // prepare SQL
+                    /*// prepare SQL
                     $sql = sprintf("SELECT max(userid) FROM UserDetails;");// Need to update this code
 
                     $result = $connection->query($sql) or die(mysqli_error()); 
                     $currentUser = mysqli_fetch_row($result);
                     $newUser = $currentUser[0]+1;
 
-                    mysqli_autocommit($connection,FALSE);
+                    mysqli_autocommit($connection,FALSE);*/
 		    
-		    $email ="ash.kulkarni1990@gmail.com"; // Need to update this code as of noe its hardcoded
-		    // Updated <19 APR 2017> Setting up session variables
-		    session_start(); // session start
-                    $_SESSION['s_userid']=$newUser ;
-                    $_SESSION['s_emailid']=$email ;
-                   
-                    $insert_usr = sprintf("INSERT INTO UserDetails (created_date) VALUES (NOW());
-                                            ");
+		            //$email ="ash.kulkarni1990@gmail.com"; // Need to update this code as of noe its hardcoded
+		            // Updated <19 APR 2017> Setting up session variables
+		            session_start(); // session start
                     
-                    // execute query
-                    $sucess = $connection->query($insert_usr) or die(mysqli_error($connection));  
+                    $newUser= $_SESSION['s_userid'];
+                    //$_SESSION['s_emailid']=$email ;
+                    $s_testid = $_SESSION['s_testid'];
+                    
+                     /*
                     if ($sucess === false)
-                        die("User not inserted into database");
+                        die("User not inserted into database");*/
                     
                     //mysqli_commit($connection);
                     
@@ -46,9 +41,12 @@
                     $original_array = unserialize($_POST['ArrayData']);
                     //var_dump($original_array);
                     
-                    if (is_array($original_array)) {
+                    
+                    if (is_array($original_array)) 
+                    {
                         
-                        for ($row = 1; $row < $_POST['sizeOfQSet'] + 1; $row++) {
+                        for ($row = 1; $row < $_POST['sizeOfQSet'] + 1; $row++) 
+                        {
                                 $testid = $original_array[$row][0];
                                 $qusid = $original_array[$row][1];
                                 $ans = $_POST['selection_'.$qusid];
@@ -68,17 +66,29 @@
                     }
 		    
 		// Updated <19 APR 2017> Making entry in DB which will trigger the calculation  :Start
-			    $insert_entry =  sprintf("INSERT INTO TestDetails (testid, userid, created_date) VALUES
+			   if ($s_testid==1)
+                          {
+                          $insert_entry =  sprintf("INSERT INTO TestDetails (testid, userid, created_date) VALUES
                                                  ('".htmlspecialchars($testid,ENT_QUOTES)."','".htmlspecialchars($newUser,ENT_QUOTES)."', now());
                                                     ");
+                          }
+                          else
+                          {
+                           $insert_entry =  sprintf("INSERT INTO Custom_TestDetails (testid, userid, created_date) VALUES
+                                                 ('".htmlspecialchars($testid,ENT_QUOTES)."','".htmlspecialchars($newUser,ENT_QUOTES)."', now());
+                                                    ");   
+                              
+                          }
                             $sucess = $connection->query($insert_entry) or die(mysqli_error($connection)); 
                           
                             if ($sucess === false)
-				    die("Assessment details not inserted into database");
+				            die("Assessment details not inserted into database");
                               
                             mysqli_commit($connection);
+                            
+                            mysqli_close($connection);
 	        // Updated <19 APR 2017> Making entry in DB which will trigger the calculation   :End    
-                mysqli_close($connection);
+                
             }
                         
             catch(PDOException $e)
@@ -92,9 +102,13 @@
             {
                 //header("Location: confirmation.php");
 		// Updated <19 APR 2017> 
-		header("Location: result.html"); 
-                exit;
+	           
+                    	    
+                    		header("Location: result.html"); 
+                    	
+                    	
+            exit;
                 
             }
-        }
+    }
 ?>
