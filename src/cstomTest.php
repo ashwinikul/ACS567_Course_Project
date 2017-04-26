243233ad3b64004083d4a34c341a216af650b77b
ashwinikul <kulka02@students.ipfw.edu>
2017-04-26 15:52:53 -0400

    Add files via upload
    Final Code Upload

--- src/cstomTest.php
+++ src/cstomTest.php
@@ -1,6 +1,9 @@
 <?php
+    //	Developer: Ashwini Kulkarni
+    //	Description: Display All Available test
+
             require_once('db_conn.php');
-            //require_once('test.php');
+
 
             
              try {
@@ -8,13 +11,13 @@
                 
                 // prepare SQL
                 $sql = sprintf("SELECT DISTINCT testid, testname
-                                    From Dim_Test where testname <> 'MBTI' ORDER BY  testname;
+                                    From Dim_Test  where testname <> 'MBTI' ORDER BY  testname;
                                     ");
                 $result = $connection->query($sql) or die(mysqli_error());
                
                session_start(); // session start
 
-
+                    echo "<br><div align= 'center'><h2><a href='sessionSet.php?c_testid=1'>MBTI TEST</a><h2></div><br><br>";    
                     echo "<br><br><div align= 'center'  style = 'font: normal 20px/150% Arial, Helvetica, sans-serif;'><fieldset  style='width: 50%;'>Dear Visiter,<br>Following assessments are made by users. They are not related to our main provided assessment 'MBTI'. Feel free to take following Test. Have fun.........!</fieldset></div><br><br>";
                     echo "<div align= 'center' style = 'font: normal 20px/150% Arial, Helvetica, sans-serif;'> -: Customized Assessments :- </div>";
                     echo "<div class='datagrid' style = 'font: normal 12px/150% Arial, Helvetica, sans-serif; background: #fff; overflow: hidden;'>";
@@ -24,6 +27,7 @@
                 $count =1;
                while ($row =  $result->fetch_assoc()) {
                     $id = $row['testid']; 
+                    echo $id;
                     if ($count%2 ==0)
                     {
                        echo "<tr class='alt'  style = 'color: #7D7D7D; font-size: 13px;border-bottom: 1px solid #E1EEF4;font-weight: normal; '><td>".$row['testname']."</td><td><a href='sessionSet.php?c_testid=".$id."' class='link_path' data-target='test' id=".$id.">Take Test</a>";
@@ -35,14 +39,7 @@
                     
                     }
                     $count=$count +1;
-                    
-                    //echo "".$row['testname']."</li><br>";
-                    //echo "</td><td>";
-                    //echo "<br><li class='link_path'><a href='test.php' >Take Test</a></li><br>";
-                    echo "</td><tr>";
-                    //$url= "test.php?c_testid=" + $id;
-                    //echo "<br><li><a href='test.php' data-target='test' class='link_path' id=".$id.">".$row['testname']."</a></li><br>";
-                   // echo "<br><li><a href='test.php?c_testid=".(string)$row['testid']."' > . $row['testname'] .</a></li><br>";
+                   echo "</td><tr>";
                    
                 }
                 echo "</tbody></table></div>";
--- src/testresult.php
+++ src/testresult.php
@@ -1,15 +1,16 @@
 <?php
-    require_once('db_conn.php');
+//	Developer: Ashwini Kulkarni
+//	Description: Fetch result from DB and Display it in proper format  PHP & JS
     session_start(); // session start
     $s_testid = $_SESSION['s_testid']; // session get
     $s_userid = $_SESSION['s_userid'];
     $s_emailid = $_SESSION['s_emailid'];
     
-   
-    
+    require_once('db_conn.php');
+ 
     $connection = connect_to_db();
     
-    if ($s_testid ==1)
+    if ($s_testid ==1)  // For MBTI
     {
                 // prepare SQL
                 $sql = sprintf("SELECT testid, userid, E_I, S_N, T_F, J_P, E_I_type, S_N_type, T_F_type, J_P_type, res, E, I, S, N, T, F, J, P FROM User_MBTI_Result WHERE 
@@ -17,7 +18,7 @@
                                 LIMIT 1 ;
                                 ");
     }
-    else
+    else // For Other Tests
     {
                 $sql = sprintf("SELECT test_result FROM Custom_Test_Result where userid='".htmlspecialchars($s_userid,ENT_QUOTES)."' and testid= '".htmlspecialchars($s_testid,ENT_QUOTES)."' LIMIT 1;");// Need to update this code
   
@@ -26,37 +27,35 @@
                                     
     // execute query
     $result = $connection->query($sql) or die(mysqli_error());   
-    //echo "<div>This is result page<div>";
-    //echo "<div><br>" .$s_testid. "<br> ".$s_userid." <br>".$s_emailid."<br></div>";
     
-    
-    
-    $stack = array();
     echo "<div id='resultMBTI' align='center'>" ;
-    if ($s_testid ==1)
+    if ($s_testid ==1) // For MBTI Graph representation
     {
+        $stack = array();
+        //echo "Inside MBTI Result";
+        echo "<div><h2>MBTI Assessment Result<h2></div>";
         echo "<div style ='background-color:white'><canvas width='800px' height='265px' id='graphs' ></canvas><div><br><br>";
         while($row = $result->fetch_assoc()) {
         array_push($stack, $row["E"],$row["I"],$row["S"],$row["N"],$row["T"],$row["F"],$row["J"],$row["P"]); 
         echo "<div><textarea name='resultMBTI' id='resultMBTI' rows='5' cols='60'  readonly style='border:none; background-color: white; '> Result : ".$row["res"]." </textarea><br /></div>";
         $datapoints =json_encode($stack,JSON_NUMERIC_CHECK);
         }
+       
     }
-    else
+    else  // For Other Tests
     {
+        echo "<div><h2>Custom Assessment Result<h2></div>";
         while($row = $result->fetch_assoc()) 
         {
-        echo "<div><textarea name='resultCust' id='resultCust' rows='5' cols='60'  readonly style='border:none; background-color: white; '> Result : ".$row["test_result"]." </textarea><br /></div>";
+      
+			echo "<h3>".$row["test_result"]."</h3>";
         }
     }
-   // echo "<div> <legend>Send result to your mail</legend><div><input type='text' name='email_id' size='60' ><br><input type='submit' name='email' value='Send Email' onSubmit="" class='send_email_bt' /></div></div></td></tr>";
     echo "</div>";
-    
-
 
 ?>
 <script type="text/javascript" src="jQuery/RGraph.common.core.js"></script>
-        <script type="text/javascript" src="jQuery/RGraph.bar.js"></script>
+<script type="text/javascript" src="jQuery/RGraph.bar.js"></script>
 
 <script type="text/javascript">
             
